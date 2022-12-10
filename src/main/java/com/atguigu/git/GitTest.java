680a609d263c74566f8370ec4b24f4381488ac55
sgtqz <110594144+sgtqz@users.noreply.github.com>
2022-12-10 17:11:18 +0800

    pull commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,6 +6,6 @@ public class GitTest {
         System.out.println("hello git2!");
         System.out.println("hello git4!");
         System.out.println("push test!");
-
+        System.out.println("pull test!");
     }
 }
